4749b2216755d4f45c5ee50c99187ae7770e287e
Cristian Botocan <c.a.botocan@student.tudelft.nl>
2021-05-04 14:48:04 +0200

    CREATING THE LOAD TESTS ACTION
    - The LoadTestAction class is implemented
    - Whenever the user press the + button, it willhave the list of the test from the selected file on the right pannel
    - Moreover, this action can be find in the "Help|Find Action" option
    - The user can use the keyboard shortcut: <first-keystroke="control alt A" second-keystroke="C">
    - The methods contains JavaDoc and the code is proper formatted.

--- src/main/kotlin/com/testbuddy/views/actions/LoadTestAction.kt
+++ src/main/kotlin/com/testbuddy/views/actions/LoadTestAction.kt
@@ -2,6 +2,8 @@ package com.testbuddy.views.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.components.service
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.ui.components.JBLabel
@@ -9,6 +11,7 @@ import com.intellij.ui.components.JBPanelWithEmptyText
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.components.JBViewport
 import com.intellij.ui.content.impl.ContentImpl
+import com.testbuddy.services.LoadTestsService
 import java.awt.Dimension
 import javax.swing.Box
 import javax.swing.BoxLayout
@@ -36,13 +39,17 @@ class LoadTestAction : AnAction {
     constructor(text: String?, description: String?, icon: Icon?) : super(text, description, icon)
 
     /**
-     * Updates the CopyPaste tab to add new test cases.
+     * Updates the CopyPaste tab to load the test cases from the selected file.
      * Has suppression for current skeleton code.
      *
      * @param event Event received when the associated menu item is chosen.
      */
     @Suppress("MagicNumber")
     override fun actionPerformed(event: AnActionEvent) {
+
+        //Take the load test service and use the get tests.
+        val loadTestsService = event.project!!.service<LoadTestsService>()
+        val listMethods = loadTestsService.getTests(event.getData(CommonDataKeys.PSI_FILE)!!)
 
         val window: ToolWindow? = ToolWindowManager.getInstance(event.project!!).getToolWindow("TestBuddy")
 
@@ -55,23 +62,27 @@ class LoadTestAction : AnAction {
         val copyPasteViewport = copyPasteScroll.getComponent(0) as JBViewport
         val copyPastePanel = copyPasteViewport.getComponent(0) as JPanel
 
-        val mPanel = JPanel()
+        for (method in listMethods) {
 
-        mPanel.layout = BoxLayout(mPanel, BoxLayout.LINE_AXIS)
+            // Create the panel for each test method
 
-        // Create the labels and buttons.
-        // The glue adds spacing/moves the button to the right
-        mPanel.add(JBLabel("Test case 0"))
-        mPanel.add(Box.createHorizontalGlue())
-        mPanel.add(JButton("Copy"))
-        mPanel.add(JButton("Goto"))
+            val mPanel = JPanel()
 
-        // Limit size of the panel
-        mPanel.minimumSize = Dimension(0, 50)
-        mPanel.maximumSize = Dimension(Integer.MAX_VALUE, 50)
-        mPanel.setSize(-1, 50)
+            mPanel.layout = BoxLayout(mPanel, BoxLayout.LINE_AXIS)
 
-        copyPastePanel.add(mPanel)
+            // Create the labels and buttons.
+            // The glue adds spacing/moves the button to the right
+
+            mPanel.add(JBLabel(method.name))
+            mPanel.add(Box.createHorizontalGlue())
+            mPanel.add(JButton("Copy"))
+            mPanel.add(JButton("Goto"))
+            // Limit size of the panel
+            mPanel.minimumSize = Dimension(0, 50)
+            mPanel.maximumSize = Dimension(Integer.MAX_VALUE, 50)
+            mPanel.setSize(-1, 50)
+            copyPastePanel.add(mPanel)
+        }
     }
 
     /**
