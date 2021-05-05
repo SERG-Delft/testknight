a27e8b68d078b07e9c18d9e4bf6d81ab599cf075
Pavlos Makridis <p.makridis@student.tudelft.nl>
2021-05-05 13:51:44 +0200

    Add info field in SideEffect

--- src/main/kotlin/com/testbuddy/models/SideEffect.kt
+++ src/main/kotlin/com/testbuddy/models/SideEffect.kt
@@ -1,6 +1,6 @@
 package com.testbuddy.com.testbuddy.models
 
-abstract class SideEffect
+open class SideEffect(val info: String)
 
-data class ThrowsExceptionSideEffect(val nameOfException: String) : SideEffect()
-data class MutatesClassFieldSideEffect(val nameOfField: String) : SideEffect()
+data class ThrowsExceptionSideEffect(val nameOfException: String) : SideEffect(nameOfException)
+data class MutatesClassFieldSideEffect(val nameOfField: String) : SideEffect(nameOfField)
