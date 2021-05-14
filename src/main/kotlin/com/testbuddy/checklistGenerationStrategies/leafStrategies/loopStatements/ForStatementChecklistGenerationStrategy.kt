941325a56fb4955caba7662b390c7853b2869657
Piyush Deshmukh <p.a.deshmukh@student.tudelft.nl>
2021-05-14 16:48:08 +0200

    Add ChecklistGenerationStrategy to for and while loops

--- src/main/kotlin/com/testbuddy/checklistGenerationStrategies/leafStrategies/loopStatements/ForStatementChecklistGenerationStrategy.kt
+++ src/main/kotlin/com/testbuddy/checklistGenerationStrategies/leafStrategies/loopStatements/ForStatementChecklistGenerationStrategy.kt
@@ -1,19 +1,33 @@
 package com.testbuddy.com.testbuddy.checklistGenerationStrategies.loopStatements
 
 import com.intellij.psi.PsiForStatement
+import com.testbuddy.com.testbuddy.checklistGenerationStrategies.leafStrategies.ConditionChecklistGenerationStrategy
 import com.testbuddy.com.testbuddy.checklistGenerationStrategies.leafStrategies.LeafChecklistGeneratorStrategy
 import com.testbuddy.com.testbuddy.models.TestingChecklistLeafNode
 
-class ForStatementChecklistGenerationStrategy :
+class ForStatementChecklistGenerationStrategy private constructor(
+    private val conditionChecklistGenerator: ConditionChecklistGenerationStrategy
+) :
     LeafChecklistGeneratorStrategy<PsiForStatement> {
 
     companion object Factory {
+
         fun create(): ForStatementChecklistGenerationStrategy {
-            return ForStatementChecklistGenerationStrategy()
+            val conditionChecklistGenerator = ConditionChecklistGenerationStrategy.create()
+            return create(conditionChecklistGenerator)
         }
+
+        fun create(conditionChecklistGenerator: ConditionChecklistGenerationStrategy):
+            ForStatementChecklistGenerationStrategy {
+                return ForStatementChecklistGenerationStrategy(conditionChecklistGenerator)
+            }
     }
 
     override fun generateChecklist(psiElement: PsiForStatement): List<TestingChecklistLeafNode> {
-        return emptyList()
+
+        val condition = psiElement.condition!!
+        val mcdcChecklist = conditionChecklistGenerator.generateChecklist(condition)
+        return mcdcChecklist +
+            listOf(TestingChecklistLeafNode("Test where for loop runs multiple times", psiElement))
     }
 }
