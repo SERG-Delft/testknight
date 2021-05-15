90eb88d9920870b04c421703fa80b7027a619910
Piyush Deshmukh <p.a.deshmukh@student.tudelft.nl>
2021-05-15 14:37:24 +0200

    Refactor GenerateTestCaseChecklistService

--- src/main/kotlin/com/testbuddy/services/GenerateTestCaseChecklistService.kt
+++ src/main/kotlin/com/testbuddy/services/GenerateTestCaseChecklistService.kt
@@ -8,15 +8,4 @@ class GenerateTestCaseChecklistService {
     var classChecklistGenerationStrategy = ClassChecklistGenerationStrategy.create()
     var methodChecklistGenerationStrategy = MethodChecklistGenerationStrategy.create()
 
-//    fun generateChecklist(file: PsiFile, editor: Editor): TestingChecklist {
-//        val caret = editor.caretModel.primaryCaret
-//        val offset = caret.offset
-//        val element = file.findElementAt(offset)
-//        val containingMethod = PsiTreeUtil.getParentOfType(element, PsiMethod::class.java)
-//
-//        return if (containingMethod != null) {
-//            generateChecklist(containingMethod)
-//            TODO()
-//        } else { TODO() } // empty checklist }
-//    }
 }
