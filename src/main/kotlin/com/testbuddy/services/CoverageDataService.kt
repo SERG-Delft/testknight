193551c3c2e84392cb5c73188a27d0ec2989b7ae
Piyush Deshmukh <p.a.deshmukh@student.tudelft.nl>
2021-05-20 17:11:01 +0200

    Implement functionality to get all classes in project and map for diff object

--- src/main/kotlin/com/testbuddy/services/CoverageDataService.kt
+++ src/main/kotlin/com/testbuddy/services/CoverageDataService.kt
@@ -3,6 +3,8 @@ package com.testbuddy.services
 import com.intellij.coverage.CoverageDataManager
 import com.intellij.coverage.CoverageSuitesBundle
 import com.intellij.openapi.project.Project
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.searches.AllClassesSearch
 import com.intellij.rt.coverage.data.ProjectData
 
 class CoverageDataService {
@@ -11,6 +13,7 @@ class CoverageDataService {
     private var previousSuite: CoverageSuitesBundle? = null
     private var currentData: ProjectData? = null
     private var currentSuite: CoverageSuitesBundle? = null
+    // private var classCoveragesMap = mutableMapOf<String, CoverageDiffObject>()
 
     /**
      * Updates coverage at the end of every execution of runWithCoverage.
@@ -24,6 +27,7 @@ class CoverageDataService {
         currentData = newData
         currentSuite = newSuite
     }
+
     /**
      * Get the current coverage data for the current suite.
      *
@@ -33,5 +37,25 @@ class CoverageDataService {
         val covDataManager = CoverageDataManager.getInstance(project)
         val suite = covDataManager.currentSuitesBundle
         return suite.coverageData
+    }
+
+    /**
+     * getDiff for both types of supported views
+     */
+    fun getDiffLines(project: Project) {
+//        previousData.classes["Point"].lines
+
+        if (previousData == null || currentData == null) { return }
+
+        // gets all relevant classes in current project (not imports and org.junit or javax classes)
+        val classesInProject = AllClassesSearch.search(GlobalSearchScope.projectScope(project), project)
+            .findAll().mapNotNull { it.name }
+
+        print(classesInProject[0])
+
+//        classesInProject.forEach {
+//            if(previousData.classes != null) {
+//            if(previousData.classes.contains(it))
+//        }
     }
 }
