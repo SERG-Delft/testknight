5550ef1db22863977024c7ff8230538fa480f44d
Pavlos Makridis <p.makridis@student.tudelft.nl>
2021-05-26 23:10:21 +0200

    Add design documents for API, client and DB

--- src/main/kotlin/com/testbuddy/TestAction.kt
+++ src/main/kotlin/com/testbuddy/TestAction.kt
@@ -1,30 +1,11 @@
 package com.testbuddy
 
-import com.intellij.coverage.CoverageDataManager
-import com.intellij.coverage.CoverageRunner
-import com.intellij.coverage.JavaCoverageEngine
-import com.intellij.coverage.JavaCoverageRunner
-import com.intellij.execution.RunManager
-import com.intellij.execution.testframework.JavaTestLocator
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
 
 class TestAction : AnAction() {
 
     override fun actionPerformed(event: AnActionEvent) {
-        val psiFile = event.getData(CommonDataKeys.PSI_FILE)!!
-        val editor = event.getData(CommonDataKeys.EDITOR)!!
-        val project = event.getData(CommonDataKeys.PROJECT)!!
-
-        val jloc = JavaTestLocator.INSTANCE
-        val runMgr = RunManager.getInstance(project)
-        val covDataMgr = CoverageDataManager.getInstance(project)
-
-        val covEngine = covDataMgr.currentSuitesBundle.coverageEngine
-
-        val covRunner = CoverageRunner.getInstance(JavaCoverageRunner::class.java)
-
-        covEngine.getTestsForLine(project, "Point", 10)
+        TODO()
     }
 }
