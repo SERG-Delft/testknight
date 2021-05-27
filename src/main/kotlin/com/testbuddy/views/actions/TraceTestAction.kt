cdfec50fef53cc8ead281c59d2016b6b4d1f9028
Jorge Romeu Huidobro <j.romeuhuidobro@student.tudelft.nl>
2021-05-27 11:13:11 +0200

    Make the intention move to the method declaration

--- src/main/kotlin/com/testbuddy/views/actions/TraceTestAction.kt
+++ src/main/kotlin/com/testbuddy/views/actions/TraceTestAction.kt
@@ -4,13 +4,19 @@ import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
 import com.intellij.openapi.components.service
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.OpenFileDescriptor
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiIdentifier
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.refactoring.suggested.startOffset
 import com.testbuddy.services.TestAnalyzerService
+import com.testbuddy.services.TestTracingService
 
 class TraceTestAction : PsiElementBaseIntentionAction(), IntentionAction {
 
@@ -31,11 +37,33 @@ class TraceTestAction : PsiElementBaseIntentionAction(), IntentionAction {
     }
 
     override fun invoke(project: Project, editor: Editor?, element: PsiElement) {
+
+        // get current test file information
         val parentMethod = PsiTreeUtil.getParentOfType(element, PsiMethod::class.java)
+        val parentClass = PsiTreeUtil.getParentOfType(element, PsiClass::class.java)
         val methodCall = PsiTreeUtil.getParentOfType(element, PsiMethodCallExpression::class.java)
 
-        if (parentMethod == null || methodCall == null)  return
+        if (parentMethod == null || methodCall == null || parentClass == null) return
 
+        // get the coverage data
+        val testName = "${parentClass.name},${parentMethod.name}"
+        val testTracingService = project.service<TestTracingService>()
+        val covData = testTracingService.getLinesForTest(testName)
+        val lines = covData.classes[parentClass.qualifiedName]
+
+        // get method declaration information
         val methodDeclaration = methodCall.resolveMethod()
+        val classDefined = PsiTreeUtil.getParentOfType(methodDeclaration, PsiClass::class.java)
+        val fileDefined = PsiTreeUtil.getParentOfType(methodDeclaration, PsiFile::class.java)
+
+        if (methodDeclaration == null || classDefined == null || fileDefined == null) return
+
+        // get declaration descriptor
+        val fileEditorManager = FileEditorManager.getInstance(project)
+        val virtualFile = fileDefined.virtualFile
+        val descriptor = OpenFileDescriptor(project, virtualFile, methodDeclaration.startOffset)
+
+        // open method declaration
+        fileEditorManager.openTextEditor(descriptor, true)
     }
 }
