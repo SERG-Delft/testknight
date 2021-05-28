c0073350df61ac4b4fc8939e1f031c4881d12dc7
Mathanrajan Sundarrajan <m.sundarrajan@student.tudelft.nl>
2021-05-28 10:59:28 +0200

    Create data structure for settings

--- src/main/kotlin/com/testbuddy/settings/SettingsState.kt
+++ src/main/kotlin/com/testbuddy/settings/SettingsState.kt
@@ -1,14 +1,34 @@
 package com.testbuddy.settings
 
-data class SettingsState(var myFlag : Boolean = false,
-                         var myString: String = "",
-                         var myInt: Int = 0,
-                         var myStringChoice: String = "") {
+import java.awt.Color
 
-    fun copyTo(copyTo : SettingsState) {
-        copyTo.myFlag = this.myFlag
-        copyTo.myString = this.myString
-        copyTo.myInt = this.myInt
-        copyTo.myStringChoice = this.myStringChoice
-    }
-}
+data class SettingsState(
+    var telemetrySettings: TelemetrySettings = TelemetrySettings(),
+    var testListSettings: TestListSettings = TestListSettings(),
+    var checklistSettings: ChecklistSettings = ChecklistSettings(),
+    var coverageSettings: CoverageSettings = CoverageSettings()
+)
+
+data class TelemetrySettings(var isEnabled: Boolean = false)
+
+data class TestListSettings(
+    var autoUpdateUI: Boolean = true,
+    var highlightStrategies: MutableMap<String, Boolean> = mutableMapOf()
+)
+
+data class ChecklistSettings(
+    var coverageCriteria: String = "MCDC",
+    var checklistStrategies: MutableMap<String, Boolean> = mutableMapOf(),
+    var typeCaseMap: MutableMap<String, List<String>> = mutableMapOf(),
+    var showGutterIcons: Boolean = true,
+    var gotoChecklistItem: Boolean = true,
+    var highlightChecklistItem: Boolean = true,
+    var highlightColor: Color = Color.LIGHT_GRAY
+)
+
+data class CoverageSettings(
+    var includedColor: Color = Color.GREEN,
+    var deletedColor: Color = Color.RED,
+    var diffIncludedColor: Color = Color.GREEN,
+    var diffExcludedColor: Color = Color.RED
+)
