db02a40836fc5d577f0fb8cd73fd75f7ad4d5c04
Piyush Deshmukh <p.a.deshmukh@student.tudelft.nl>
2021-05-28 16:48:29 +0200

    Add filterByHitsTest for previous suite

--- src/test/kotlin/com/testbuddy/services/CoverageDataServiceTest.kt
+++ src/test/kotlin/com/testbuddy/services/CoverageDataServiceTest.kt
@@ -33,7 +33,7 @@ class CoverageDataServiceTest : BasePlatformTestCase(){
                 "simple description for line 2")
         line2.hits = 4
 
-        val line6 = LineData(6, "simple description for line 3")
+        val line6 = LineData(6, "simple description for line 6")
         line6.hits = 6
 
         val classData = mockk<ClassData>()
@@ -49,6 +49,7 @@ class CoverageDataServiceTest : BasePlatformTestCase(){
     @Test
     fun testNullLinesInClassDataPassed(){
 
+        //some null lines
         val line0 = null
         val line1 = LineData(1,
                 "simple description for line 1")
@@ -62,7 +63,7 @@ class CoverageDataServiceTest : BasePlatformTestCase(){
         val line4 = null
         val line5 = null
 
-        val line6 = LineData(6,"simple description for line 3")
+        val line6 = LineData(6,"simple description for line 6")
         line6.hits = 6
 
         val classData = mockk<ClassData>()
@@ -77,8 +78,56 @@ class CoverageDataServiceTest : BasePlatformTestCase(){
         every { classData.getLineData(5)} returns line5
         every { classData.getLineData(6)} returns line6
 
-
         TestCase.assertEquals(service.getLinesCoveredPreviously(classData),setOf(1,2,6))
+    }
+
+    @Test
+    fun testFilterByHitsCoveredPreviously(){
+
+        val line0 = LineData(0,
+                "simple description for line 0")
+        line0.hits = 0
+
+        val line1 = LineData(1,
+                "simple description for line 1")
+        line1.hits = 1
+
+        val line2 = LineData(2,
+                "simple description for line 2")
+        line2.hits = -1
+
+        val line3 = LineData(3,
+                "simple description for line 3")
+        line3.hits = Int.MAX_VALUE
+
+        val line4 = LineData(4,
+                "simple description for line 4")
+        line4.hits = Int.MIN_VALUE
+
+        val line5 = LineData(5,
+                "simple description for line 5")
+        line5.hits = 0
+
+        val line6 = LineData(6,
+                "simple description for line 6")
+        line6.hits = 2
+
+        val line7 = null
+
+        val classData = mockk<ClassData>()
+
+        every { classData.lines } returns arrayOf(line0,line1,line2,line3,line4,line5,line6,line7)
+
+        every { classData.getLineData(0)} returns line0
+        every { classData.getLineData(1)} returns line1
+        every { classData.getLineData(2)} returns line2
+        every { classData.getLineData(3)} returns line3
+        every { classData.getLineData(4)} returns line4
+        every { classData.getLineData(5)} returns line5
+        every { classData.getLineData(6)} returns line6
+        every { classData.getLineData(7)} returns line7
+
+        TestCase.assertEquals(service.getLinesCoveredPreviously(classData),setOf(1,3,6))
     }
 
     @Test
