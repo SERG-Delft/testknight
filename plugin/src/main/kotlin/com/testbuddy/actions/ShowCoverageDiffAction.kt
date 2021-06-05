2e0877a346726ffc6f77bdb50d1cd0903dec8fc1
Mathanrajan Sundarrajan <m.sundarrajan@student.tudelft.nl>
2021-06-05 18:24:46 +0200

    Create custom exception for invalid virtual file/document

--- plugin/src/main/kotlin/com/testbuddy/actions/ShowCoverageDiffAction.kt
+++ plugin/src/main/kotlin/com/testbuddy/actions/ShowCoverageDiffAction.kt
@@ -9,6 +9,8 @@ import com.intellij.openapi.ui.WindowWrapper
 import com.intellij.openapi.ui.WindowWrapperBuilder
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.table.JBTable
+import com.testbuddy.exceptions.DocumentNotFoundException
+import com.testbuddy.exceptions.InvalidVirtualFileException
 import com.testbuddy.services.CoverageDataService
 import com.testbuddy.services.CoverageHighlighterService
 import com.testbuddy.services.UsageDataService
@@ -17,48 +19,61 @@ import javax.swing.AbstractAction
 
 class ShowCoverageDiffAction(val table: JBTable, val project: Project) : AbstractAction() {
 
-    override fun actionPerformed(p0: ActionEvent?) {
-        if (p0 != null) {
-            val row = p0.actionCommand.toInt()
-            val className = table.model.getValueAt(row, 0) as String
+    /**
+     * Opens a new window which shows the diff view for coverage.
+     *
+     * @param e Event received when the associated menu item is chosen.
+     */
+    override fun actionPerformed(e: ActionEvent?) {
+        e ?: return // return if event is null
 
-            val serv = project.service<CoverageDataService>()
+        val row = e.actionCommand.toInt()
+        val className = table.model.getValueAt(row, 0) as String
 
-            // Couldn't find the file.
-            val vFile = serv.classCoveragesMap[className]?.virtualFile ?: return
+        val serv = project.service<CoverageDataService>()
 
-            if (!vFile.isValid) {
-                return
-            }
-
-            val document = FileDocumentManager.getInstance().getDocument(vFile) ?: return
-
-            val editorFactory = EditorFactory.getInstance()
-
-            val coverageHighlighterService = project.service<CoverageHighlighterService>()
-
-            val leftEditor = editorFactory.createEditor(document, null, vFile, true)
-            val rightEditor = editorFactory.createEditor(document, null, vFile, true)
-
-            coverageHighlighterService.showHighlightsInDiff(leftEditor, rightEditor, className)
-
-            val twoSidePanel = TwosideContentPanel(mutableListOf(leftEditor.component, rightEditor.component))
-
-            val scrollPanel = JBScrollPane()
-            scrollPanel.viewport.view = twoSidePanel
-            val windowWrapper = WindowWrapperBuilder(WindowWrapper.Mode.FRAME, scrollPanel)
-                .setProject(project)
-                .setTitle("Diff Coverage")
-                // releaseEditor on close.
-                .setOnCloseHandler {
-                    editorFactory.releaseEditor(leftEditor)
-                    editorFactory.releaseEditor(rightEditor)
-                    true // requires a boolean return, true after releasing editor
-                }
-                .build()
-
-            windowWrapper.show()
-            UsageDataService.instance.logSplitDiffView()
+        if (serv.classCoveragesMap[className] == null) {
+            throw InvalidVirtualFileException(className, "Class not found in coverage.")
         }
+
+        val vFile = serv.classCoveragesMap[className]!!.virtualFile
+            ?: throw InvalidVirtualFileException(className, "Virtual file is null.")
+
+        if (!vFile.isValid) {
+            throw InvalidVirtualFileException(
+                className,
+                "File (or parent) deleted or external change."
+            )
+        }
+
+        val document = FileDocumentManager.getInstance().getDocument(vFile)
+            ?: throw DocumentNotFoundException()
+
+        val editorFactory = EditorFactory.getInstance()
+
+        val coverageHighlighterService = project.service<CoverageHighlighterService>()
+
+        val leftEditor = editorFactory.createEditor(document, null, vFile, true)
+        val rightEditor = editorFactory.createEditor(document, null, vFile, true)
+
+        coverageHighlighterService.showHighlightsInDiff(leftEditor, rightEditor, className)
+
+        val twoSidePanel = TwosideContentPanel(mutableListOf(leftEditor.component, rightEditor.component))
+
+        val scrollPanel = JBScrollPane()
+        scrollPanel.viewport.view = twoSidePanel
+        val windowWrapper = WindowWrapperBuilder(WindowWrapper.Mode.FRAME, scrollPanel)
+            .setProject(project)
+            .setTitle("Diff Coverage")
+            // releaseEditor on close.
+            .setOnCloseHandler {
+                editorFactory.releaseEditor(leftEditor)
+                editorFactory.releaseEditor(rightEditor)
+                true // requires a boolean return, true after releasing editor
+            }
+            .build()
+
+        windowWrapper.show()
+        UsageDataService.instance.logSplitDiffView()
     }
 }
