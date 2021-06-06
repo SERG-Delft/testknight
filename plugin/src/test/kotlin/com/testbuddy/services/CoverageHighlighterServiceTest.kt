32bd105b7601408409cc94e0477fd8dd24043057
Piyush Deshmukh <p.a.deshmukh@student.tudelft.nl>
2021-06-06 10:26:25 +0200

    Add tests for integrated view highlights

--- plugin/src/test/kotlin/com/testbuddy/services/CoverageHighlighterServiceTest.kt
+++ plugin/src/test/kotlin/com/testbuddy/services/CoverageHighlighterServiceTest.kt
@@ -4,11 +4,13 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.markup.MarkupModel
 import com.intellij.openapi.editor.markup.RangeHighlighter
 import com.testbuddy.extensions.TestBuddyTestCase
+import io.mockk.Called
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkClass
 import io.mockk.spyk
 import io.mockk.verify
+import junit.framework.TestCase
 import org.junit.Test
 import java.awt.Color
 
@@ -44,7 +46,7 @@ class CoverageHighlighterServiceTest : TestBuddyTestCase() {
 
     @Test
     fun testHideHighlights(){
-        myFixture.configureByFile("/Methods.java")
+        myFixture.configureByFile("/Math.java")
 
         val editor = mockk<Editor>()
         val service = CoverageHighlighterService(project)
@@ -60,6 +62,40 @@ class CoverageHighlighterServiceTest : TestBuddyTestCase() {
 
         service.hideHighlights(editor)
         verify { editor.markupModel }
+    }
+
+    @Test
+    fun testHideHighlightsEmptyMapForeachNotRun(){
+        myFixture.configureByFile("/Methods.java")
+
+        val editor = mockk<Editor>()
+        val service = CoverageHighlighterService(project)
+        val rangeHighlighter = mockk<RangeHighlighter>()
+        val mutableHashMap = HashMap<Editor, MutableSet<RangeHighlighter>>()
+        val markupModel = mockk<MarkupModel>()
+
+        mutableHashMap[editor] = mutableSetOf(rangeHighlighter)
+        service.setHighlights(mutableHashMap)
+
+        every { editor.markupModel } returns markupModel
+        every { markupModel.removeHighlighter(any()) } returns Unit
+        // a different editor is passed
+        service.hideHighlights(myFixture.editor)
+        verify(inverse = true) { editor.markupModel }
+    }
+
+    @Test
+    fun testShowHighlightsNull(){
+        myFixture.configureByFile("/Methods.java")
+        val service = CoverageHighlighterService(project)
+        val covService = mockk<CoverageDataService>()
+        service.setCoverageDataService(covService)
+        every { covService.getDiffLines(any()) } returns Unit
+        every { covService.classCoveragesMap[any()] } returns null
+
+        service.showHighlights(myFixture.editor, "ClassName")
+
+        TestCase.assertEquals(covService.classCoveragesMap["ClassName"],null)
     }
 
 }
