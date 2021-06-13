6e1d825584f79ec35375fb998905e49921c3b9f8
Piyush Deshmukh <p.a.deshmukh@student.tudelft.nl>
2021-06-13 13:48:28 +0200

    Add method to aggregate test setup in TestBuddyTestCase

--- plugin/src/main/kotlin/com/testbuddy/extensions/TestBuddyTestCase.kt
+++ plugin/src/main/kotlin/com/testbuddy/extensions/TestBuddyTestCase.kt
@@ -1,7 +1,13 @@
 package com.testbuddy.extensions
 
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiMethod
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.testbuddy.settings.SettingsService
+import org.jetbrains.annotations.NotNull
 
 open class TestBuddyTestCase : BasePlatformTestCase() {
 
@@ -11,4 +17,12 @@ open class TestBuddyTestCase : BasePlatformTestCase() {
     }
 
     override fun getTestDataPath() = "testdata"
+
+    fun getBasicTestInfo(filepath: String) {
+        myFixture.configureByFile(filepath)
+        val project = myFixture.project
+        val psiClass = PsiTreeUtil.findChildOfType(myFixture.file, PsiClass::class.java)
+        val psiFile = myFixture.file
+        val testClasses = PsiTreeUtil.findChildrenOfType(psiFile, PsiClass::class.java)
+    }
 }
